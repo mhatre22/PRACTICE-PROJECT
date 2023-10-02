6e7e9da165868d5e31a6bbd605d07aa3bb9d42d7
mhatre22 <mhatresnehal75@gmail.com>
2023-10-02 20:39:49 +0530

    deleted

--- Angular_project16/src/app/app.component.ts
+++ Angular_project16/src/app/app.component.ts
@@ -3,14 +3,14 @@ import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl:'./app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Angular_project16';
   constructor(private router:Router){
   }
-    homecomponent(){
+  homecomponent(){
       this.router.navigateByUrl('/home')
 }
 aboutcomponent(){
