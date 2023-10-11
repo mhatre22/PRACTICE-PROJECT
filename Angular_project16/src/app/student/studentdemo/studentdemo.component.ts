dd0901df97230a0d288279e72e99a7d9f27e9b5a
mhatre22 <mhatresnehal75@gmail.com>
2023-10-11 23:12:13 +0530

    pass valid

--- Angular_project16/src/app/student/studentdemo/studentdemo.component.ts
+++ Angular_project16/src/app/student/studentdemo/studentdemo.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormBuilder,Validator, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 
 @Component({
@@ -9,23 +9,69 @@ import { FormGroup, FormBuilder,Validator, Validators } from '@angular/forms';
 })
 export class StudentdemoComponent {
 
-signInForm!: FormGroup
+  SignInForm!: FormGroup
 showSignInForm : boolean =false;
+passwordValue : any;
+confirmpasswordValue:any;
+isMatch!: boolean;
+isMacthConfirm!: boolean;
+pass: any;
+confirmpass: any;
+showpassword! : boolean;
 constructor(private formBuilder: FormBuilder){}
 showForm(){
-this.showSignInForm = true;
-this.signInForm = this.formBuilder.group({
+//this.showSignInForm = true;
+this.showSignInForm =! this.showSignInForm
+this.formDef();
+}
+formDef(){
+
+
+this.SignInForm = this.formBuilder.group({
   fullname : ['',[Validators.required, Validators.pattern("[a-zA-Z]*$"), Validators.minLength(10)]],
   mobilenumber : ['',[Validators.required, Validators.pattern("[0-9]*$"),Validators.minLength(10),Validators.maxLength(10)]],
-  password:['',[Validators.required, Validators.pattern("[[A-Z]{1}[a-zA-Z0-9@#!$%&^*]{8}$]"), Validators.minLength(9),Validators.maxLength(10)]],
-  confirmpassword:['',[Validators.required, Validators.pattern("[[A-Z]{1}[a-zA-Z0-9@#!$%&^*]{8}$]"), Validators.minLength(9)]]
+  password:[''],
+  confirmpassword:[''],
+  name : ['',[Validators.required,this.nameValidation,this.whitespaceValidation]]
 })
 }
-
-
-
-submit(formData:any){
-  console.log(formData)
+whitespaceValidation(name:any){
+  let data=name.value;
+  let newdata = data?.trim();
+  let isValid = data.length !=newdata.length;
+  return isValid ? {whitespace:true}: null
+}
+nameValidation(inp:any){
+  console.log(inp.value);
+  let data = inp.value?.toLowerCase();
+  let isErr = data.includes('copy')
+  return isErr ?{Err : true}:null
 
 }
+passwordValidation(inp:any){
+  console.log(inp.value);
+  console.log(this.pass);
 }
+
+password(){
+  this.passwordValue = this.SignInForm.value.password;
+  if(this.passwordValue == this.confirmpasswordValue){
+    this.isMatch = true;
+  }
+  else{
+    this.isMatch= false;
+  }
+}
+confirmpassword(){
+  this.confirmpasswordValue = this.SignInForm.value.confirmpassword;
+  if(this.passwordValue == this.confirmpasswordValue){
+    this.isMacthConfirm == true;
+  }
+  else{
+    this.isMacthConfirm=false;
+  }
+}
+showPass(){
+  this.showpassword = !this.showpassword;
+
+}}
